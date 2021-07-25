e30db599ce9c075ed3bf5a91d70dcbead2d6bb2a
matthewnavarro2 <matthew10navarro@gmail.com>
2021-07-24 23:42:07 -0400

    FriendSection changes

--- frontend/src/components/friends/FriendsSection.js
+++ frontend/src/components/friends/FriendsSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -9,21 +9,28 @@ function FriendsSection()
 {
 
   var bp = require('../Path.js');
-  var storage = require('../../tokenStorage.js');
+    var storage = require('../../tokenStorage.js');
+    const jwt = require("jsonwebtoken");
+    
+    var search = '';
 
-  var _ud = localStorage.getItem('user_data');
-  var ud = JSON.parse(_ud);
-  var userId = ud.id;
-
-  let searchUserName = '';
-  let searchUsersArray = [];
-
-  const searchUsers = async event => 
+    const [message,setMessage] = useState('');
+    const [runList,setRunList] = useState('');
+    
+    var _ud = localStorage.getItem('user_data');
+    var ud = JSON.parse(_ud);
+    var userId = ud.id;
+    var list = [];
+    var nameList;
+    var fullname;
+    var userId_toadd;
+  
+    const searchUser = async event => 
     {
         event.preventDefault();
         		
         var tok = storage.retrieveToken();
-        var obj = {userId:userId,search:'',jwtToken:tok};
+        var obj = {search:search.value,jwtToken:tok};
         var js = JSON.stringify(obj);
 
         var config = 
@@ -47,22 +54,33 @@ function FriendsSection()
     
             if( res.error.length > 0 )
             {
-                // setMessage( "API Error:" + res.error );
+                setMessage( "API Error:" + res.error );
             }
             else
             {
                 var fullNameResults = res.results;
                 var resultText = '';
-
-                fullNameResults.forEach( (userObject, index) => {
-                    if ( (userObject.FullName.toLowerCase()).includes( searchUserName.value.toLowerCase() ) )
+                // grabbing this so i can use it later.
+                
+                for(var i = 0; i < fullNameResults.length; i++)
+                {
+                    fullname = fullNameResults[i].FullName;
+                    userId_toadd = fullNameResults[i].UserId;  
+                    if ( (fullname.toLowerCase()).includes( search.value.toLowerCase() ))
                     {
-                        console.log(userObject.FullName)
-                        searchUsersArray.push(userObject.FullName);
+                        resultText += fullname + '\n';
+                        list[i] = fullNameResults[i];
+                        
                     }
-                    
-                });
-                console.log(searchUsersArray)
+
+                }
+                console.log(list);
+                nameList = list.map((element) => <p className="display-users" key = {element.UserId}> {element.FullName} <Button variant="primary" className="search-user-buttons" id="addUserButton"
+                onClick={searchUser, addFriend} > Add Friend </Button></p>); 
+
+                
+                setMessage('User(s) have been retrieved');
+                setRunList(nameList);
                 storage.storeToken( {accessToken:retTok} );
             }
         })
@@ -70,29 +88,78 @@ function FriendsSection()
         {
             console.log(error);
         });
+        
     };
+
+    const addFriend = async event => 
+    {
+      event.preventDefault();
+
+    var tok = storage.retrieveToken();
+    var obj = {userId:userId, userId_toadd:userId_toadd,  jwtToken:tok};
+    var js = JSON.stringify(obj);
+    console.log(userId_toadd);
+    var config = 
+    {
+        method: 'post',
+        url: bp.buildPath('../../../../api/addfriend'),	
+        headers: 
+        {
+            'Content-Type': 'application/json'
+        },
+        data: js
+    };
+
+    axios(config)
+        .then(function (response) 
+    {
+        var res = response.data;
+        console.log(res)
+        if (res.error) 
+        {
+            setMessage('User/Password combination incorrect');
+        }
+        else 
+        {
+          var res = response.data;
+          var retTok = res.jwtToken;
+  
+          if( res.error.length > 0 )
+          {
+              setMessage( "API Error:" + res.error );
+          }
+          else
+          {
+              setMessage('You have made a new friend!');
+              storage.storeToken( {accessToken:retTok} );
+          }
+          
+        }
+    })
+    .catch(function (error) 
+    {
+        console.log(error);
+    });
+  }
 
   return(
    <Container className="friends-page-subsection-add-friend leaderboard">
+     <p>{message}</p>
      <Row className="friends-search-add">
         <Col sm={8}>
-          <input type="text" id="search-user-input" placeholder="Search user" ref={(c) => searchUserName = c} />
+          <input type="text" id="search-user-input" id="run-text" placeholder="Search user" 
+                ref={(c) => search = c} />
         </Col>
-        <Col sm={3}>
-          <Button variant="primary" style={{borderRadius: "8px", height: "32px"}} onClick={searchUsers}>
-            Add friend +
-          </Button>
+        <Col sm={4}>
+          <Button variant="primary" className="search-run-buttons" id="addRunButton"
+                onClick={searchUser}> Search User </Button>
         </Col>   
      </Row>
-        <Col>
-          
-        </Col>
-     <Row>
-
-     </Row>
-       {/* <div className="friends-search-add">
        
-       </div> */}
+     <Row>
+          <Col >{runList} </Col>
+     </Row>
+       
    </Container>
   );
 
